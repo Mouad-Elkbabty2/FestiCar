fae1450fd39402c30da4f80c4853a400d4a6b6a9
Mouad elkbabty <mouadelkbabty@gmail.com>
2024-01-15 21:58:20 +0100

    First commit yeey

---
+++ server/src/main/java/fr/uga/m1miage/example/ExampleSpringApplication.java
@@ -0,0 +1,11 @@
+package fr.uga.m1miage.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringApplication.class, args);
+    }
+}
