48b39b8534ee15beda4b09ce940c2924fae74b28
Mouad elkbabty <mouadelkbabty@gmail.com>
2024-02-01 11:39:43 +0100

    Cors origin Content

--- server/src/main/java/fr/uga/m1miage/example/ExampleSpringApplication.java
+++ server/src/main/java/fr/uga/m1miage/example/ExampleSpringApplication.java
@@ -2,6 +2,9 @@ package fr.uga.m1miage.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -10,5 +13,14 @@ public class ExampleSpringApplication {
 
 
         SpringApplication.run(ExampleSpringApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+        };
     }
 }
